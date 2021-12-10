8b95769d076423674c2d091d4ed274fb79715943
nadaabdelgayed <75578900+nadaabdelgayed@users.noreply.github.com>
2021-12-10 15:35:05 +0200

    calculating remaining time in process
    missing remaining time initialization

--- Starter Code/process.c
+++ Starter Code/process.c
@@ -1,7 +1,7 @@
 #include "headers.h"
 
 /* Modify this file as needed*/
-int remainingtime;
+int remainingtime ;
 
 int main(int agrc, char *argv[])
 {
@@ -9,8 +9,19 @@ int main(int agrc, char *argv[])
 
     //TODO it needs to get the remaining time from somewhere
     //remainingtime = ??;
+    int prevtime = 0;
     while (remainingtime > 0)
     {
+        clock_t cycles = clock();
+        int time = (double)cycles / CLOCKS_PER_SEC;
+        if (time - prevtime == 1)
+        {
+            remainingtime--;
+        }
+
+        printf(" time elapsed %d\n",
+               time);
+        prevtime = time;
         // remainingtime = ??;
     }
 
