37bec82971a0d4875ca73079b6ae18a063867268
Ziyad Sameh <ziyad.ss@hotmail.com>
2021-12-17 16:15:34 +0200

    Update process.c

--- src/process.c
+++ src/process.c
@@ -2,6 +2,11 @@
 
 int main(int argc, char *argv[])
 {
-    while (clock() < atoi(argv[1]) * CLOCKS_PER_SEC);
+    if (argc != 2)
+        return fprintf(stderr, "Incorrect usage, to run use: ./bin/process.out <time>\n");
+
+    int time = atoi(argv[1]);
+    while (clock() < time * CLOCKS_PER_SEC);
+
     return 0;
 }
